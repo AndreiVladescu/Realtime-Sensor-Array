3c8a0c12fba4f17787a380d12291476791acea11
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2022-10-01 21:10:58 +0300

    Hardcoded Static IP

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def data_transmission():
         # Socket Create
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
+        host_ip = '192.168.100.200' # socket.gethostbyname(host_name)
         print('HOST IP:', host_ip)
         port = 9999
         socket_address = (host_ip, port)
